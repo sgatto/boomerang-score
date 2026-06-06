d1459174f650b8384bb334257dfd7bfaef51adf2
klahaebe-cmyk <klahaebe@gmail.com>
2026-06-06 17:10:47 +0200

    - Fixed FC scoring - introduced Tapir scoring

--- src/boomerang_score/core/scorer.py
+++ src/boomerang_score/core/scorer.py
@@ -124,20 +124,18 @@ def _points_50(result):
 
 def _points_fc(result):
     _max_time = 60.0
-    if result == 0:
-        points = 0
-    elif result == 1:
-        points = 387.26
-    elif result == 2:
-        points = 518.71
-    elif result == 3:
-        points = 600.01
-    elif result == 4:
-        points = 659.03
-    elif result >= 75:
-        points = 659.03
-    elif result >= 5:
-        points = 500 * math.log10(1 + 99 * (15.00 / float(result)))
+    _min_time = 15.00
+    _laps = 5
+    if result >= 75: 
+        #complete round maxtime cut off
+        points = 500 * math.log10(1 + 99 * (_min_time / 75.00))
+    elif result >= 5: 
+        #complete round
+        points = 500 * math.log10(1 + 99 * (_min_time / float(result)))
+    elif result > 0:
+         #non complete round, points scaled by laps completed
+         #for non complete rounds the score is the number of the catches
+         points = 500 * math.log10(1 + 99 * (_min_time / _max_time *(float(result) / _laps)))
     else:
         points = -200
     return points
@@ -165,10 +163,26 @@ def _points_timed(result):
 
 
 def _points_tapir(result):
-    try:
-        return float(result) * 3
-    except (TypeError, ValueError):
-        return 0.0
+    _max_time = 300.0
+    _min_time = 90.00
+    _laps = 0.85
+    _maxtime_cut_off = float(_max_time*_laps/(_laps-0.01))
+    if result >= _maxtime_cut_off:
+        #complete round maxtime cut off
+        points = 500 * math.log10(1 + 99 * (_min_time / _maxtime_cut_off))
+    elif result >= 0.85:
+        #complete round
+        points = 500 * math.log10(1 + 99 * (_min_time / float(result)))
+    elif result > 0:
+        #non complete round, points scaled by laps completed
+        #each ACC point is 0.01 points
+        #each TC is 0.05 points related to the acc points for each TC
+        #0.05 points for each FC
+        #so total for 4 Fast Catches is 0.8 points
+        points = 500 * math.log10(1 + 99 * (_min_time / _max_time *(float(result) / _laps)))
+    else:
+        points = -200
+    return points
 
 
 ACC = Discipline(DISC_CODE_ACC, DISC_LABEL_ACC, True, lambda e: _points_100(float(e)))
